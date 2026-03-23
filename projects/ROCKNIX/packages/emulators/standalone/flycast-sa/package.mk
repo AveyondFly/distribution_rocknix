470c93451d1ed418b74faed7add1549def60e2c0
kk <405437445@qq.com>
2026-03-23 16:23:02 +0800

    flycast-sa: bump version and add Chinese UI support
    - Update to latest commit with Chinese i18n improvements
    - Add patches for complete Chinese translations (zh_CN, zh_TW, zh_HK)
    - Add custom Chinese font path support for Linux

--- projects/ROCKNIX/packages/emulators/standalone/flycast-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/flycast-sa/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="flycast-sa"
-PKG_VERSION="392a429e8b040b3e5bf6696cb4f984274fc44123" #v2.6
+PKG_VERSION="05b270f05cecfcd675bb0530cf18d0a9b81269a1" #v2.6
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="${PKG_SITE}.git"

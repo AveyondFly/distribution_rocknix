467072c5977437ce9490b01c9aaf50886b84fdb9
kk <405437445@qq.com>
2026-04-25 11:57:09 +0800

    image: move gamepadtester to emulator

--- projects/ROCKNIX/packages/virtual/emulators/package.mk
+++ projects/ROCKNIX/packages/virtual/emulators/package.mk
@@ -26,6 +26,8 @@ LIBRETRO_CORES="81-lr a5200-lr arduous-lr atari800-lr b2-lr beetle-gba-lr beetle
                 snes9x-lr snes9x2002-lr snes9x2005_plus-lr snes9x2010-lr stella-lr swanstation-lr tgbdual-lr theodore-lr tic80-lr  \
                 uzem-lr vba-next-lr vbam-lr vecx-lr vice-lr vircon32-lr virtualjaguar-lr xmil-lr wasm4-lr yabasanshiro-lr fbneoplus-lr"
 
+PKG_DEPENDS_TARGET+=" gamepadtester"
+
 ### Emulators or cores for specific devices
 case "${DEVICE}" in
   H700)

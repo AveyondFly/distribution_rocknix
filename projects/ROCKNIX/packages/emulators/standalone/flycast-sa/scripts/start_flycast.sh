9485058ffcd6fce5b20167746bf9c40f2f12e28d
kk <405437445@qq.com>
2026-05-07 09:55:22 +0800

    flycast-sa: update cfg

--- projects/ROCKNIX/packages/emulators/standalone/flycast-sa/scripts/start_flycast.sh
+++ projects/ROCKNIX/packages/emulators/standalone/flycast-sa/scripts/start_flycast.sh
@@ -48,12 +48,7 @@ fi
 #Emulation Station Features
 GAME=$(echo "${1}"| sed "s#^/.*/##")
 PLATFORM=$(echo "${2}"| sed "s#^/.*/##")
-ASPECT=$(get_setting aspect_ratio "${PLATFORM}" "${GAME}")
-ASKIP=$(get_setting auto_frame_skip "${PLATFORM}" "${GAME}")
-FPS=$(get_setting show_fps "${PLATFORM}" "${GAME}")
-IRES=$(get_setting internal_resolution "${PLATFORM}" "${GAME}")
 GRENDERER=$(get_setting graphics_backend "${PLATFORM}" "${GAME}")
-VSYNC=$(get_setting vsync "${PLATFORM}" "${GAME}")
 
 #Set the cores to use
 CORES=$(get_setting "cores" "${PLATFORM}" "${GAME}")
@@ -68,42 +63,6 @@ else
   unset EMUPERF
 fi
 
-  #AspectRatio
-        if [ "$ASPECT" = "w" ]; then
-                sed -i '/^rend.WideScreen =/c\rend.WideScreen = yes' "${CONF_DIR}/${FLYCAST_INI}"
-                sed -i '/^rend.SuperWideScreen =/c\rend.SuperWideScreen = no' "${CONF_DIR}/${FLYCAST_INI}"
-        elif [ "$ASPECT" = "sw" ]; then
-                sed -i '/^rend.WideScreen =/c\rend.WideScreen = yes' "${CONF_DIR}/${FLYCAST_INI}"
-                sed -i '/^rend.SuperWideScreen =/c\rend.SuperWideScreen = yes' "${CONF_DIR}/${FLYCAST_INI}"
-	else
-		sed -i '/^rend.WideScreen =/c\rend.WideScreen = no' "${CONF_DIR}/${FLYCAST_INI}"
-		sed -i '/^rend.SuperWideScreen =/c\rend.SuperWideScreen = no' "${CONF_DIR}/${FLYCAST_INI}"
-        fi
-
-  #AutoFrameSkip
-        if [ "$ASKIP" = "normal" ]; then
-                sed -i '/^pvr.AutoSkipFrame =/c\pvr.AutoSkipFrame = 1' "${CONF_DIR}/${FLYCAST_INI}"
-        elif [ "$ASKIP" = "max" ]; then
-                sed -i '/^pvr.AutoSkipFrame =/c\pvr.AutoSkipFrame = 2' "${CONF_DIR}/${FLYCAST_INI}"
-	else
-		sed -i '/^pvr.AutoSkipFrame =/c\pvr.AutoSkipFrame = 0' "${CONF_DIR}/${FLYCAST_INI}"
-        fi
-
-  #Internal Resolution
-        if [ "$IRES" = "0" ]; then
-                sed -i '/rend.Resolution =/c\rend.Resolution = 240' "${CONF_DIR}/${FLYCAST_INI}"
-        elif [ "$IRES" = "2" ]; then
-                sed -i '/rend.Resolution =/c\rend.Resolution = 720' "${CONF_DIR}/${FLYCAST_INI}"
-        elif [ "$IRES" = "3" ]; then
-                sed -i '/rend.Resolution =/c\rend.Resolution = 960' "${CONF_DIR}/${FLYCAST_INI}"
-        elif [ "$IRES" = "4" ]; then
-                sed -i '/rend.Resolution =/c\rend.Resolution = 1200' "${CONF_DIR}/${FLYCAST_INI}"
-        elif [ "$IRES" = "5" ]; then
-                sed -i '/rend.Resolution =/c\rend.Resolution = 1440' "${CONF_DIR}/${FLYCAST_INI}"
-        else
-                sed -i '/rend.Resolution =/c\rend.Resolution = 480' "${CONF_DIR}/${FLYCAST_INI}"
-        fi
-
   #Graphics Renderer
         if [ "$GRENDERER" = "opengl" ]; then
                 sed -i '/^pvr.rend =/c\pvr.rend = 0' "${CONF_DIR}/${FLYCAST_INI}"
@@ -113,20 +72,6 @@ fi
 		sed -i '/^pvr.rend =/c\pvr.rend = @GRENDERER@' "${CONF_DIR}/${FLYCAST_INI}"
 	fi
 
-  #ShowFPS
-        if [ "$FPS" = "1" ]; then
-                sed -i '/^rend.ShowFPS =/c\rend.ShowFPS = yes' "${CONF_DIR}/${FLYCAST_INI}"
-	else
-		sed -i '/^rend.ShowFPS =/c\rend.ShowFPS = no' "${CONF_DIR}/${FLYCAST_INI}"
-        fi
-
-  #Vsync
-        if [ "$VSYNC" = "1" ];then
-                sed -i '/^rend.vsync =/c\rend.vsync = yes' "${CONF_DIR}/${FLYCAST_INI}"
-	else
-		sed -i '/^rend.vsync =/c\rend.vsync = no' "${CONF_DIR}/${FLYCAST_INI}"
-        fi
-
 #Retroachievements
 /usr/bin/cheevos_flycast.sh
 
@@ -135,12 +80,7 @@ fi
   echo "PLATFORM set to: ${PLATFORM}"
   echo "CONF DIR: ${CONF_DIR}/${FLYCAST_INI}"
   echo "CPU CORES set to: ${EMUPERF}"
-  echo "ASPECT RATIO set to: ${ASPECT}"
-  echo "AUTO FRAME SKIP set to: ${ASKIP}"
-  echo "INTERNAL RESOLUTION set to: ${IRES}"
   echo "GRAPHICS RENDERER set to: ${GRENDERER}"
-  echo "FPS set to: ${FPS}"
-  echo "VSYNC set to: ${VSYNC}"
   echo "Launching /usr/bin/flycast ${1}"
 
 CORE=${3%-*}

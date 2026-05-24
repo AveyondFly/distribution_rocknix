44d270a7da2a60e30e4f70288843fc89be7286a8
kk <405437445@qq.com>
2026-05-24 23:11:27 +0800

    linux-uapi: fix uapi dep

--- projects/ROCKNIX/packages/linux-uapi/package.mk
+++ projects/ROCKNIX/packages/linux-uapi/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
 PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/linux-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_HOST="openssl:host"
+PKG_DEPENDS_HOST="ccache:host openssl:host"
 PKG_LONGDESC="Linux UAPI headers for userspace (sysroot), independent of the target kernel version."
 
 make_host() {
@@ -15,8 +15,23 @@ make_host() {
 }
 
 makeinstall_host() {
+  local headers_arch="${HEADERS_ARCH}"
+  if [ -z "${headers_arch}" ]; then
+    case "${TARGET_ARCH}" in
+      aarch64)
+        headers_arch="arm64"
+        ;;
+      x86_64)
+        headers_arch="x86"
+        ;;
+      *)
+        headers_arch="${TARGET_ARCH}"
+        ;;
+    esac
+  fi
+
   make \
-    ARCH=${HEADERS_ARCH:-${TARGET_KERNEL_ARCH}} \
+    ARCH=${headers_arch} \
     HOSTCC="${TOOLCHAIN}/bin/host-gcc" \
     HOSTCXX="${TOOLCHAIN}/bin/host-g++" \
     HOSTCFLAGS="${HOST_CFLAGS}" \

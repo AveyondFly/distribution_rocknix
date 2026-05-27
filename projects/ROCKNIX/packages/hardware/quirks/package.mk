ce49dea70d8fddbd3940368ab1653e8f1730001b
kk <kk@ubuntu>
2026-05-27 17:02:02 +0800

    S905L3A: restore ACP HDMI audio path

--- projects/ROCKNIX/packages/hardware/quirks/package.mk
+++ projects/ROCKNIX/packages/hardware/quirks/package.mk
@@ -36,12 +36,6 @@ post_install() {
   if [ "${DEVICE}" = "S905L3A" ]; then
     enable_service persist-mac.service
     enable_service s905l3a-audio.service
-    mkdir -p ${INSTALL}/usr/share/pipewire/pipewire.conf.d
-    cp ${PKG_DIR}/platforms/S905L3A/pipewire/*.conf \
-      ${INSTALL}/usr/share/pipewire/pipewire.conf.d/
-    mkdir -p ${INSTALL}/usr/share/pipewire/pipewire-pulse.conf.d
-    cp ${PKG_DIR}/platforms/S905L3A/pipewire-pulse/*.conf \
-      ${INSTALL}/usr/share/pipewire/pipewire-pulse.conf.d/
     mkdir -p ${INSTALL}/usr/share/wireplumber/wireplumber.conf.d
     cp ${PKG_DIR}/platforms/S905L3A/wireplumber/51-sc2-alsa-hdmi.conf \
       ${INSTALL}/usr/share/wireplumber/wireplumber.conf.d/

8efad8d2a3e35bf69226be2edf731b204f909eb8
aveyond <msuanming@163.com>
2026-06-20 06:50:32 +0800

    Change fbneoplus-lr version to specific commit
    Updated package version to a specific commit hash.

--- projects/ROCKNIX/packages/emulators/libretro/fbneoplus-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/fbneoplus-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="fbneoplus-lr"
-PKG_VERSION="master"
+PKG_VERSION="ce0aae7135d83badf0be59f8babc55853d10b194"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/lrf739146825/FBNeo"
 PKG_URL="${PKG_SITE}.git"

178a267e99abb4c9351329d18ecfddd590066583
lcdyk <lcdyk0517@qq.com>
2026-06-19 21:59:08 +0800

    add rk356x ppsspp-2021

--- projects/ROCKNIX/packages/virtual/emulators/package.mk
+++ projects/ROCKNIX/packages/virtual/emulators/package.mk
@@ -66,6 +66,7 @@ case "${DEVICE}" in
     PKG_EMUS+=" box64 dolphin-sa drastic-sa drastic_adv-sa mednafen melonds-sa portmaster scummvmsa yabasanshiro-sa duckstation-sa"
     LIBRETRO_CORES+=" dolphin-lr flycast2021-lr geolith-lr uae4arm"
     PKG_RETROARCH+=" retropie-shaders"
+    PKG_EMUS+=" ppsspp2021-sa"
     ;;
   RK3588)
     [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 desmume-lr gpsp-lr gpsp_ezode-lr pcsx_rearmed-lr"

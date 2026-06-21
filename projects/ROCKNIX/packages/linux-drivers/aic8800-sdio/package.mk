6efb49883e679e662ccb2d8fccc3fbebb364de65
kk <405437445@qq.com>
2026-06-21 19:37:12 +0800

    rk3566: add aic8800 for raxda-zero-3w

---
+++ projects/ROCKNIX/packages/linux-drivers/aic8800-sdio/package.mk
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="aic8800-sdio"
+PKG_VERSION="fda6713f1eebdc83ce23d9f35b6dec1abae2b2c9"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/radxa-pkg/aic8800"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_LONGDESC="AIC8800 SDIO WiFi out-of-tree driver (aic8800_bsp + aic8800_fdrv)"
+PKG_TOOLCHAIN="manual"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  kernel_make -C $(kernel_path) \
+    M=${PKG_BUILD}/src/SDIO/driver_fw/driver/aic8800 \
+    CONFIG_PLATFORM_UBUNTU=n \
+    CONFIG_AIC_FW_PATH=/lib/firmware/aic8800 \
+    CONFIG_AIC8800_BTLPM_SUPPORT=n \
+    modules
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+  cp ${PKG_BUILD}/src/SDIO/driver_fw/driver/aic8800/aic8800_bsp/aic8800_bsp.ko \
+     ${PKG_BUILD}/src/SDIO/driver_fw/driver/aic8800/aic8800_fdrv/aic8800_fdrv.ko \
+     ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+
+  mkdir -p ${INSTALL}/$(get_full_firmware_dir)/aic8800
+  cp ${PKG_BUILD}/src/SDIO/driver_fw/fw/aic8800*/* \
+     ${INSTALL}/$(get_full_firmware_dir)/aic8800/
+}

99b9bb2d8e36e5ca895aa683a0493399e3d8e04b
kk <405437445@qq.com>
2026-07-06 11:30:06 +0800

    emu: optimize ekal21

--- projects/ROCKNIX/packages/emulators/standalone/eka2l1-sa/scripts/start_eka2l1.sh
+++ projects/ROCKNIX/packages/emulators/standalone/eka2l1-sa/scripts/start_eka2l1.sh
@@ -9,6 +9,11 @@ mkdir -p "$EKA2L1_DIR/data"
 
 gptokeyb  -k "eka2l1_sdl2" -c ./eka2l1.gptk  --killsignal 15&
 
+if [ ! -e "/storage/.config/eka2l1/data" ]; then
+    mkdir -p /roms/eka2l1/data
+    ln -sf /roms/eka2l1/data  /storage/.config/eka2l1/
+fi
+
 CUBEB_BACKEND=alsa ./eka2l1_sdl2
 
 kill -9 `pidof gptokeyb`

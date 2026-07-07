c18e23824b811fa7d9a74d9493d5825a353c0bbe
kk <405437445@qq.com>
2026-07-07 10:16:01 +0800

    emu: fix j2me hardcode lang

--- projects/ROCKNIX/packages/emulators/standalone/free-j2me/sources/start_freej2me.sh
+++ projects/ROCKNIX/packages/emulators/standalone/free-j2me/sources/start_freej2me.sh
@@ -30,8 +30,6 @@ chmod -R 755 ./.java
 export SDL_GAMECONTROLLERCONFIG_FILE=/storage/.config/SDL-GameControllerDB/gamecontrollerdb.txt
 gptokeyb -k sdl_interface -c ./j2me.gptk --killsignal 15&
 
-export LANG="zh_CN.UTF-8"
-
 #JAVA_TOOL_OPTIONS='-Xverify:none -Djava.util.prefs.systemRoot=/storage/roms/savestates/j2me/ -Djava.util.prefs.userRoot=/storage/roms/savestates/j2me/ -Djava.awt.headless=true -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -Djava.library.path=/storage/java/lib'
 JAVA_TOOL_OPTIONS='-Xverify:none -Djava.awt.headless=true -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -Djava.library.path=/storage/.config/java/lib'
 export JAVA_TOOL_OPTIONS

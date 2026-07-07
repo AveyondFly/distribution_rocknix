1a07de2493f3954647976d0af1d7eb3bca4265bf
kk <405437445@qq.com>
2026-07-07 09:51:41 +0800

    eka2l1: fix extra mkdir

--- projects/ROCKNIX/packages/emulators/standalone/eka2l1-sa/scripts/start_eka2l1.sh
+++ projects/ROCKNIX/packages/emulators/standalone/eka2l1-sa/scripts/start_eka2l1.sh
@@ -4,9 +4,6 @@
 EKA2L1_DIR="/storage/.config/eka2l1/"
 cd "$EKA2L1_DIR"
 
-# Data directory (config, devices, ROMs) lives alongside the binary
-mkdir -p "$EKA2L1_DIR/data"
-
 gptokeyb  -k "eka2l1_sdl2" -c ./eka2l1.gptk  --killsignal 15&
 
 if [ ! -e "/storage/.config/eka2l1/data" ]; then
